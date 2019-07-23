e8fd1ecd70ccfa3b410ec9b243eab45dda095a9c
Jhon Jairo Zabala <jhonnyzb1@hotmail.com>
2019-07-23 09:37:35 -0500

    modificaciones en los modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { LoginComponent } from './login/login.component';
-import { DasboardComponent } from './pages/dasboard/dasboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 const routes: Routes = [
-  {
-    path: '', component: PagesComponent,
-    children: [
-      {
-        path: 'dashboard', component: DasboardComponent
-      },
-      {
-        path: 'progress', component: ProgressComponent
-      },
-      {
-        path: 'graficas1', component: Graficas1Component
-      },
-      {
-        path: '', redirectTo: '/dashboard', pathMatch: 'full'
-      }
-    ]
-  },
-
   {
     path: 'login', component: LoginComponent
   },
